9474a717f54283a137890b6a717719373de08f2f
Edu1Sinico <EduardoSenai888@gmail.com>
2025-08-08 22:06:37 -0300

    Inserindo a primeira aula de POO do segundo semestre - 08/08/2025

---
+++ Segundo_Semestre/Programacao_Orientada_Objeto/Primeiro-Projeto/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+
+        int a = 5;
+        int b = 7;
+        int soma = a + b;
+        System.out.println("A soma é " + soma + ".");
+
+    }
+}
