4d801938ba074d212a97baab89fbccfdbbed85aa
Edu1Sinico <EduardoSenai888@gmail.com>
2025-09-04 22:04:19 -0300

    Enviando o conteúdo de semântica web para o Github - 04/09/2025

--- Segundo_Semestre/Programacao_Orientada_Objeto/tarefa_01_formativa_poo/ex_01_pares_impares/src/ValidarParImpar.java
+++ Segundo_Semestre/Programacao_Orientada_Objeto/tarefa_01_formativa_poo/ex_01_pares_impares/src/ValidarParImpar.java
@@ -3,16 +3,17 @@ public class ValidarParImpar {
     private int impar;
 
     // Método com estrutura IF
-    public void ValidarParImparIF(int numero){
+    public void ValidarParImparIF(int numero) {
         if (numero % 2 == 0) {
             System.out.println("O número '" + numero + "' é par.");
         } else {
-            System.out.println("O número '" + numero + "' é impar.");        }
+            System.out.println("O número '" + numero + "' é impar.");
+        }
     }
 
     // Método com estrutura Switch Case
-    public void ValidarParImparSwitch(int numero){
-        switch (numero%2){
+    public void ValidarParImparSwitch(int numero) {
+        switch (numero % 2) {
             case 0:
                 System.out.println("O número '" + numero + "' é par.");
                 break;
