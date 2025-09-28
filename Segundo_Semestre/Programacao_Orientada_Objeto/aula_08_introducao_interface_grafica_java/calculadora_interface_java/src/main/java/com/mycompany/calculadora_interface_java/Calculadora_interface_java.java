f786ce240ca0fffa6be920eadcf61a4e0b0e380e
Eduardo Sinico <123770640+Edu1Sinico@users.noreply.github.com>
2025-09-27 23:17:59 -0300

    Delete Segundo_Semestre/Programacao_Orientada_Objeto/aula_08_introducao_interface_grafica_java/calculadora_interface_java directory

--- Segundo_Semestre/Programacao_Orientada_Objeto/aula_08_introducao_interface_grafica_java/calculadora_interface_java/src/main/java/com/mycompany/calculadora_interface_java/Calculadora_interface_java.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.calculadora_interface_java;
-
-public class Calculadora_interface_java {
-
-    public static void main(String[] args) {
-       Principal p = new Principal();
-       p.setVisible(true);
-    }
-}
