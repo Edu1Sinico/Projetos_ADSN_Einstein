18b1e1adf6a0e782f62eaeab00815ee9a645985a
Edu1Sinico <EduardoSenai888@gmail.com>
2025-09-27 23:00:59 -0300

    Enviando o projeto de estrutura de dados para o Github - 27/09/2025

---
+++ Segundo_Semestre/Programacao_Orientada_Objeto/aula_08_introducao_interface_grafica_java/calculadora_interface_java/src/main/java/com/calculadora/Calculadora.java
@@ -0,0 +1,9 @@
+package com.mycompany.calculadora_interface_java;
+
+public class Calculadora_interface_java {
+
+    public static void main(String[] args) {
+       Principal p = new Principal();
+       p.setVisible(true);
+    }
+}
