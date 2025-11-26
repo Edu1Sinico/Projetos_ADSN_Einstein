b77e5a1f5dcb96aa3539ee6c92571765c75c5b3f
Edu1Sinico <EduardoSenai888@gmail.com>
2025-11-25 22:09:56 -0300

    Salvando o arquivo para o Github - 25/11/2025

--- Segundo_Semestre/Analise_de_Dados_Business_intelligence/projeto_hackaton/tratamentoExcelReestruturado.py
+++ Segundo_Semestre/Analise_de_Dados_Business_intelligence/projeto_hackaton/tratamentoExcelReestruturado.py
@@ -2,7 +2,7 @@
 # IMPORTAÇÕES
 # ============================================
 import pandas as pd
-
+from sqlalchemy import create_engine
 
 # ============================================
 # CONFIGURAÇÕES OPCIONAIS DE VISUALIZAÇÃO
@@ -44,7 +44,7 @@ def preparar_tabela(df, nome_coluna_principal, colunas_remover):
 # ============================================
 # LEITURA DO EXCEL
 # ============================================
-todas_as_planilhas = pd.read_excel("eOuve - Limeira.xlsx", sheet_name=None)
+todas_as_planilhas = pd.read_excel("\eOuve - Limeira.xlsx", sheet_name=None)
 
 secretaria = todas_as_planilhas["Secret_categ"]
 assuntos = todas_as_planilhas["Assuntos_categ"]
@@ -52,6 +52,7 @@ bairro = todas_as_planilhas["Bairro_categ"]
 tipo_categoria = todas_as_planilhas["Secret_categ"]
 tipo_status = todas_as_planilhas["Secret_status"]
 categoria_temporaria = todas_as_planilhas["Assuntos_categ"]
+manifesto_sigiloso_temp = todas_as_planilhas["SigilosPorCateg"]
 
 
 # ============================================
@@ -83,8 +84,8 @@ bairro = preparar_tabela(
 # ============================================
 # REMOVER ÚLTIMAS LINHAS LIXO
 # ============================================
-secretaria = secretaria.drop(index=[18])
-assuntos = assuntos.drop(index=[144])
+secretaria = secretaria.drop(index=[1, 18])
+assuntos = assuntos.drop(index=[1, 144])
 bairro = bairro.drop(index=[270])
 
 
@@ -93,10 +94,12 @@ bairro = bairro.drop(index=[270])
 # ============================================
 tipo_categoria.columns = tipo_categoria.iloc[0]
 tipo_categoria = tipo_categoria[1:]
+tipo_categoria = tipo_categoria.drop(columns=["Secretaria", "Total"])
 tipo_categoria = pd.DataFrame({"Nome_Tipo_Categoria": tipo_categoria.columns})
 
 tipo_status.columns = tipo_status.iloc[0]
 tipo_status = tipo_status[1:]
+tipo_status = tipo_status.drop(columns=["Secretaria", "Total"])
 tipo_status = pd.DataFrame({"Nome_Tipo_Status": tipo_status.columns})
 
 
@@ -134,11 +137,11 @@ assuntos = assuntos.drop(columns=["Nome_Secretaria"])
 categoria_temporaria.columns = categoria_temporaria.iloc[0]
 categoria_temporaria = categoria_temporaria[1:]
 categoria_temporaria = categoria_temporaria.drop(columns=["Assunto","Total"])
-categoria_temporaria = categoria_temporaria.drop(index=[144])
+categoria_temporaria = categoria_temporaria.drop(index=[1,144])
 
 # Formato longo
 categoria_long = categoria_temporaria.reset_index().rename(columns={"index": "ID_Assunto"})
-categoria_long["ID_Assunto"] = categoria_long["ID_Assunto"] + 1
+categoria_long["ID_Assunto"] = categoria_long["ID_Assunto"] - 1
 
 categoria_long = categoria_long.melt(
     id_vars=["ID_Assunto"],
@@ -171,6 +174,63 @@ categoria = categoria_long[[
 ]].sort_values(["ID_Assunto", "ID_Tipo_Categoria"])
 
 
+
+
+
+# ============================================
+# CRIAÇÃO TABELA FATO MANIFESTO SIGILOSO
+# ============================================
+
+manifesto_sigiloso_temp.columns = manifesto_sigiloso_temp.iloc[0]
+manifesto_sigiloso_temp = manifesto_sigiloso_temp[1:]
+manifesto_sigiloso_temp = manifesto_sigiloso_temp.drop(columns=["Secretaria","Total"])
+manifesto_sigiloso_temp = manifesto_sigiloso_temp.drop(index=[1,144])
+
+manifesto_long = manifesto_sigiloso_temp.reset_index().rename(columns={"index": "ID_Assunto"})
+manifesto_long["ID_Assunto"] = manifesto_long["ID_Assunto"] - 1
+
+manifesto_long = manifesto_long.melt(
+    id_vars=["ID_Assunto"],
+    var_name="Nome_Tipo_Categoria",
+    value_name="Quantidade"
+)
+
+manifesto_long = manifesto_long[manifesto_long["Quantidade"] > 0]
+
+# Adicionar ID_Tipo_Categoria
+manifesto_long = manifesto_long.merge(
+    tipo_categoria,
+    on="Nome_Tipo_Categoria",
+    how="left"
+)
+
+# Adicionar ID_Secretaria
+manifesto_long = manifesto_long.merge(
+    assuntos[["ID_Assunto", "ID_Secretaria"]],
+    on="ID_Assunto",
+    how="left"
+)
+
+# Tabela fato final
+manifesto_sigiloso = manifesto_long[[
+    "ID_Assunto",
+    "ID_Secretaria",
+    "ID_Tipo_Categoria",
+    "Quantidade"
+]].sort_values(["ID_Assunto", "ID_Tipo_Categoria"])
+
+
+
+
+
+# ============================================
+# CRIAÇÃO DA TABE LA FATO STATUS
+# ============================================
+
+
+
+
+
 # ============================================
 # EXIBIR RESULTADOS
 # ============================================
@@ -191,3 +251,11 @@ print(tipo_status)
 
 print("\nTabela FATO - Categoria:")
 print(categoria)
+
+print("\nTabela FATO - Manifesto Sigiloso:")
+print(manifesto_sigiloso)
+
+# ============================================
+# IMPORTAÇÃO DAS TABELAS PARA O SQL SERVER
+# ============================================
+
